cef69ff5ee132e6ec3a3921d588ccdc56e6daa75
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2025-02-10 10:25:43 +0200

    added auth component

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./authContext";
 import { Slot } from "expo-router";
 
 export default function App() {
